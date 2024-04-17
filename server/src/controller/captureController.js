460c6218193686ffaa2c5f0d45ec690e726f1acd
salove16 <shashank.shalabh909@gmail.com>
2024-04-17 16:57:44 +0530

    updated capture logic

--- server/src/controller/captureController.js
+++ server/src/controller/captureController.js
@@ -1,37 +1,42 @@
 const data = require("../db.json")
 
 // Simulate fugitive's location
-const fugitiveLocation = data.cities[Math.floor(Math.random() * data.cities.length)];
+// const fugitiveLocation = data.cities[Math.floor(Math.random() * data.cities.length)];
+
+const checkCpature=(copsArray,fugitiveCity)=>{
+    for (let i = 0; i < copsArray.length; i++) {
+        if (copsArray[i].selectedCity === fugitiveCity) {
+            return [true,copsArray[i]]
+        }
+    }
+    return [false];
+}
+
+
 
 exports.capture = async (req, res) => {
     try {
-       const result=[] 
-      const { copCity, copVehicle } = req.body;
+      const copsArray= req.body;
+
+
+     const fugitiveLocation = data.cities[Math.floor(Math.random() * data.cities.length)];
       
-  
-      const copRange = data.vehicles.find(
-        (vehicle) => vehicle.kind === copVehicle
-      ).range;
+
+const result=checkCpature(copsArray,fugitiveLocation.name)
+
+if(result[0]){
+    const capturingCop = data.cops.find((cop)=>cop.name===result[1].cop)
+    const copLocation = data.cities.find((city) => city.name === result[1].selectedCity);
+
+    const cop_Vehicle=data.vehicles.find((vehicle)=>vehicle.kind === result[1].selectedVehicle)
+    res.status(200).json({ success: true, result:[capturingCop,cop_Vehicle,copLocation] });
+
+}else{
+    res.status(200).json({ success: false });
+}
+
+   
     
-      const copLocation = data.cities.find((city) => city.name === copCity);
-
-const cop_Vehicle=data.vehicles.find((vehicle)=>vehicle.kind === copVehicle)
-
-      const distanceToFugitive = Math.abs(
-        fugitiveLocation.distance - copLocation.distance
-      );
-
-      
-      const successfulCapture = distanceToFugitive <= copRange;
-      
-      if (successfulCapture) {
- 
-        const capturingCop = data.cops[Math.floor(Math.random() * data.cops.length)];
-        result.push(capturingCop,cop_Vehicle,copLocation)
-        res.status(200).json({ success: true, result });
-      } else {
-        res.status(200).json({ success: false });
-      }
     } catch (err) {
       res.status(500).json(err);
     }
